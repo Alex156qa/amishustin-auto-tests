134ccbe6b3d9e9222a9e068312d2723777d1c55a
alex156qa <alex15696@gmail.com>
2023-09-17 20:51:41 +0300

    Add SearchTests

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -15,10 +15,17 @@ public class MainPage {
     public SelenideElement mainYourProfile =  $(byText("Your profile"));
     public SelenideElement mainYourNickname =  $(".p-nickname");
     public SelenideElement mainErrorAuthor =  $(".flash.flash-full.flash-error");
-    public SelenideElement mainSearchInput =  $(".AppHeader-searchButton.form-control.input-contrast");
+    public SelenideElement mainSearchWWInput =  $(".AppHeader-searchButton.form-control.input-contrast");
     public SelenideElement mainLinkBranches =  $(".file-navigation.mb-3.d-flex.flex-items-start .ml-3.Link--primary.no-underline .octicon.octicon-git-branch");
     public SelenideElement mainBranchesFixtures =  $("[branch=\"fixtures\"] .branch-name.css-truncate-target.v-align-baseline.width-fit.mr-2.Details-content--shown");
     public SelenideElement mainDropDownFixtures =  $(".btn.css-truncate");
+    public SelenideElement mainSearchInput =  $("#qb-input-query");
+    public SelenideElement mainSearchIncognito =  $("[data-target=\"qbsearch-input.inputButtonText\"]");
+    public SelenideElement mainSearchTextInput =  $("#query-builder-test");
+    public SelenideElement mainAuthSingIn =  $(".HeaderMenu-link.HeaderMenu-link--sign-in.flex-shrink-0.no-underline.d-block.d-lg-inline-block.border.border-lg-0.rounded.rounded-lg-0.p-2.p-lg-0");
+    public SelenideElement mainBlokRelease =  $("[data-testid=\"results-list\"]");
+    public SelenideElement mainSearchOpen =  $("#query-builder-test");
+
 
 
     public void click() {
--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -19,5 +20,39 @@ public class SearchTests {
         mainPage.mainDropDownFixtures
                 .shouldHave(text("Fixtures"));
         sleep(2000);
+    }
+
+    @Test
+    @DisplayName("Позитивные проверки поиска по релизам ")
+    public void searshTestReleases() {
+        open("https://github.com/junit-team/junit4");
+        mainPage.mainAuthSingIn.click();
+        mainPage.mainInputLog
+                .sendKeys("alex156qa");
+        mainPage.mainInputPass
+                .sendKeys("Sasha!kazan156");
+        mainPage.mainAuthorButton
+                .click();
+        mainPage.mainSearchIncognito
+                .click();
+        mainPage.mainSearchTextInput
+                .sendKeys("4.9.");
+        mainPage.mainSearchTextInput
+                .pressEnter();
+        mainPage.mainBlokRelease
+                        .shouldHave(text("doc/ReleaseNotes4.9.md"));
+        mainPage.mainSearchInput
+                        .click();
+        mainPage.mainSearchTextInput
+                .sendKeys("Summary of Changes in version 4.9, final");
+        mainPage.mainSearchTextInput
+                .pressEnter();
+        mainPage.mainBlokRelease
+                .shouldHave(text("## Summary of Changes in version 4.9, final ##\n" +
+                        "Release theme: Test-class and suite level Rules."));
+        sleep(2000);
+
+
+
     }
 }
